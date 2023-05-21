ccfc0864a610be4ab03ac082c181915781447b82
Gordon Burns <gordonburns@live.co.uk>
2023-05-21 21:25:22 +0100

    Created the pdf invoices for each CSV

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # What is this project?
 
-This is an app that creates PDF invoices from excel files
+This is an app that creates PDF invoices from excel files 
+
+The app loads the data from csv into its own data frame using pandas & glob
+Before producing a pdf invoice for each frame 
+
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import glob
 import openpyxl as pyxl
+import tabulate
 from fpdf import FPDF
 from pathlib import Path
+
 
 filepaths = glob.glob("invoices/*.xlsx")
 
@@ -16,4 +18,10 @@ for filepath in filepaths:
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=f"Invoice {invoice_num[0]}", align="L", ln=1)
+    pdf.ln(55)
+    num_columns = df.shape[1]
+    column_width = 200 / num_columns
+    data = df.to_markdown()
+    pdf.set_font(family="Courier", size=8)  # Use Courier font for fixed-width
+    pdf.multi_cell(w=200, h=20, txt=data, align="L")
     pdf.output(f"PDF_INVOICES/Invoice{invoice_num[0]}.pdf")
