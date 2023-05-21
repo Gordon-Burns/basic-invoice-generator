b178aaabc7f98d6c7d788218f1f4f176f5e5b76e
Gordon Burns <gordonburns@live.co.uk>
2023-05-21 20:00:43 +0100

    Create an MVP for each Excel file

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import glob
 import openpyxl as pyxl
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath,sheet_name="Sheet 1")
-    print(df)
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.set_auto_page_break(auto=False, margin=0)
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_num = filename.split("-")
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=f"Invoice {invoice_num[0]}", align="L", ln=1)
+    pdf.output(f"PDF_INVOICES/Invoice{invoice_num[0]}.pdf")
